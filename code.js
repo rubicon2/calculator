be9c1b3dd7ad826a3bce80f9c0acd46dc01472d5
Liam <liargent@hotmail.com>
2022-05-07 19:55:12 +0100

    Add comments to outline how the functions will work

--- code.js
+++ code.js
@@ -1,6 +1,11 @@
 const resultDisplay = document.getElementById("calc-result");
 const regex = /^\d+(\d$|[.]?\d*$)/;
 
+// Operator is a string that identifies what we'll do with the operands. 
+let operator = null;
+let operandA = 0;
+let operandB = 0;
+
 const add = function(a, b) {
     return a + b;
 }
@@ -31,12 +36,26 @@ const modulus = function() {
 
 const numericInput = function(s) {
     // Will input as a string so can check against regex, then will cast into number. 
+
+    // If operator is null, we are entering operandA. 
+
+    // If operator has been selected/pulled over from previous calculation, we are entering operandB. 
 }
 
 const setOperator = function(operationName) {
+    // operandA has been set. 
 
+    // An operator is selected.
+
+    // Now user enters operandB. 
 }
 
 const calculate = function() {
+    // Switch(operator) and calculate with operandA, operandB. 
 
+    // Store result in operandA and display. 
+
+    // Set up data for further operations on result. 
+    // operator stays the same unless changed, so numericInput is taking operandB. 
+    // operandA holds result. 
 }
