a360e9f0a33b18485513b0d24df83bfceb3ebd3c
Liam <liargent@hotmail.com>
2022-05-07 19:14:43 +0100

    Add js file with empty functions referenced in html onclick events

---
+++ code.js
@@ -0,0 +1,42 @@
+const resultDisplay = document.getElementById("calc-result");
+const regex = /^\d+(\d$|[.]?\d*$)/;
+
+const add = function(a, b) {
+    return a + b;
+}
+
+const subtract = function(a, b) {
+    return a - b;
+}
+
+const multiply = function(a, b) {
+    return a * b;
+}
+
+const divide = function(a, b) {
+    return a / b;
+}
+
+const clear = function() {
+
+}
+
+const toggleNegative = function() {
+
+}
+
+const modulus = function() {
+
+}
+
+const numericInput = function(s) {
+    // Will input as a string so can check against regex, then will cast into number. 
+}
+
+const setOperator = function(operationName) {
+
+}
+
+const calculate = function() {
+
+}
