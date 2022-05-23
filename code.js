40428b417a3ff0311c34ccc6a53e876a1dcaca46
Liam <liargent@hotmail.com>
2022-05-23 18:47:45 +0100

    Link buttons to functions, clear setOperator() and calculate() for fresh code

--- code.js
+++ code.js
@@ -1,11 +1,14 @@
-const resultDisplay = document.querySelector("#calc-result div");
-const regex = /^(?=.{1,15}$)\d+(\d$|[.]?\d*$)/;
+const resultDisplay = document.querySelector("#display div");
+// TBC regex. 
+const regex = /^(?=.{1,10}$)\d+(\d$|[.]?\d*$)/;
 
-// Operator is a string that identifies what we'll do with the operands. 
 let inputString = "";
 let operator = null;
+let previousOperator = null;
+// When ready, the input string will be converted into operands of type number. 
 let operandA = null;
 let operandB = null;
+let total = null;
 
 const add = function(a, b) {
     return a + b;
@@ -21,10 +24,6 @@ const multiply = function(a, b) {
 
 const divide = function(a, b) {
     return a / b;
-}
-
-const modulus = function(a, b) {
-    return a % b;
 }
 
 const clearData = function() {
@@ -46,6 +45,16 @@ const toggleNegativeInput = function() {
     }
 }
 
+const backspace = function() { 
+    if (inputString.length < 2) {
+        inputString = "";
+        displayOutput("0");
+    } else {
+        inputString = inputString.slice(0, inputString.length - 1);
+        displayOutput(inputString);
+    }
+}
+
 const numericInput = function(s) {
     // Will input as a string so can check against regex. 
     let newInputString = `${inputString}${s}`;
@@ -55,41 +64,9 @@ const numericInput = function(s) {
 }
 
 const setOperator = function(operationName) {
-    // operandA has been set by user, if not carried over from last operation. 
-    if (operandA === null)
-        operandA = inputString;
-        inputString = "";
-    // An operator has been selected.
-    operator = operationName; 
-    // Then user will enter operandB. 
+
 }
 
 const calculate = function() {
-    // Multiplying by 1 will cast string type into number type.
-    operandA = operandA * 1;
-    operandB = inputString * 1;
-    inputString = "";
-    // Switch(operator) and calculate with operandA, operandB. 
-    switch(operator) {
-        case "ADD":
-            operandA = add(operandA, operandB);
-            break;
-        case "SUBTRACT":
-            operandA = subtract(operandA, operandB);
-            break;
-        case "MULTIPLY":
-            operandA = multiply(operandA, operandB);
-            break;
-        case "DIVIDE": 
-            operandA = divide(operandA, operandB);
-            break; 
-        case "MODULUS":
-            operandA = modulus(operandA, operandB);
-        default:
-            break;
-    }
-    // Store result in operandA and display. 
-    displayOutput(operandA);
-    // Set up data for further operations on result. Currently selected operator will carry over. 
-    operandB = null;
+
 }
