c8e10957108e949dc5c8998ca44caf99ebfbb29d
Liam <liargent@hotmail.com>
2022-05-25 21:43:20 +0100

    Implement calculator: chain calculations, repeat last calc with '='

--- code.js
+++ code.js
@@ -1,36 +1,24 @@
 const resultDisplay = document.querySelector("#display div");
 // TBC regex. 
-const regex = /^(?=.{1,10}$)\d+(\d$|[.]?\d*$)/;
+const regex = /^(?=.{1,10}$)-?\d+(\d$|[.]?\d*$)/;
 
 let inputString = "";
-let operator = null;
+let currentOperator = null;
 let previousOperator = null;
 // When ready, the input string will be converted into operands of type number. 
-let operandA = null;
-let operandB = null;
-let total = null;
+let operandA = 0;
+let operandB = 0;
+let previousOperandB = 0;
 
-const add = function(a, b) {
-    return a + b;
-}
-
-const subtract = function(a, b) {
-    return a - b;
-}
-
-const multiply = function(a, b) {
-    return a * b;
-}
-
-const divide = function(a, b) {
-    return a / b;
-}
+let isBackspaceEnabled = true;
 
 const clearData = function() {
     inputString = "";
-    operandA = null;
-    operandB = null;
-    operator = null;
+    operandA = 0;
+    operandB = 0;
+    currentOperator = null;
+    lastOperator = null;
+    isBackspaceEnabled = true;
     displayOutput("0");
 }
 
@@ -40,22 +28,27 @@ const displayOutput = function(output) {
 
 const toggleNegativeInput = function() {
     if (inputString != "") {
-        inputString *= -1;
+        // Make sure once inputString's sign is reversed, turn back into string
+        // as numericInput() and backspace() expect strings, not numbers. 
+        inputString = (inputString * -1).toString();
         displayOutput(inputString);
     }
 }
 
 const backspace = function() { 
-    if (inputString.length < 2) {
-        inputString = "";
-        displayOutput("0");
-    } else {
+    if (isBackspaceEnabled) {
         inputString = inputString.slice(0, inputString.length - 1);
-        displayOutput(inputString);
+        if (inputString === "-" || inputString.length < 1) {
+            inputString = "";
+            displayOutput("0");
+        } else {
+            displayOutput(inputString);
+        }
     }
 }
 
 const numericInput = function(s) {
+    isBackspaceEnabled = true;
     // Will input as a string so can check against regex. 
     let newInputString = `${inputString}${s}`;
     if (regex.test(newInputString))
@@ -63,10 +56,44 @@ const numericInput = function(s) {
     displayOutput(inputString);
 }
 
-const setOperator = function(operationName) {
-
+const setOperator = function(operator) {
+    if (operandA === 0) {
+        operandA = inputString * 1;
+        inputString = "";
+    } 
+    else if (inputString != "") {
+        equals();
+        operandB = 0;
+    }
+    currentOperator = operator;
 }
 
-const calculate = function() {
+const calculate = function(operator, a, b) {
+    switch(operator) {
+        case "ADD":
+            return a += b;
+        case "SUBTRACT":
+            return a -= b;
+        case "MULTIPLY":
+            return a *= b;
+        case "DIVIDE":
+            return a /= b;
+    }
+}
 
+const equals = function() {
+    operandB = inputString * 1;
+    if (operandB != 0 && currentOperator != null) {
+        operandA = calculate(currentOperator, operandA, operandB);
+        previousOperator = currentOperator;
+        previousOperandB = operandB;
+        currentOperator = null;
+    }
+    else if (previousOperator != null) {
+        operandA = calculate(previousOperator, operandA, previousOperandB);
+    }
+    // Disable backspace so user cannot delete digits of result. Will be enabled when user starts entering a new operand.
+    isBackspaceEnabled = false;
+    inputString = ""; 
+    displayOutput(operandA);
 }
